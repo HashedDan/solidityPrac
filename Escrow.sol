6f89afb885808d06d4174cc89b5ef662dde49c24
Daniel Mangum <danielmangum@wustl.edu>
2017-09-14 08:39:34 -0500

    testing github account

---
+++ Escrow.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.11;
+
+contract Escrow {
+
+	address public buyer;
+	address public sellers;
+	address public arbiter;
+
+	// comment
+	function Escrow(address _seller, address _arbiter) {
+
+	}
+}
