af1e9e565c7a3bbb467fec0cd255dd483e272277
HashedDan <dmangum@hashedhealth.com>
2017-09-14 09:53:30 -0500

    removed test comments

--- Escrow.sol
+++ Escrow.sol
@@ -6,10 +6,6 @@ contract Escrow {
 	address public sellers;
 	address public arbiter;
 
-	// comment more and more
-	// test
-	// got it this time
-	// now
 	function Escrow(address _seller, address _arbiter) {
 
 	}
