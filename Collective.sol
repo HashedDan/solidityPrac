6956ceb17c3911443ebbb60b5a371dfb95ec31f3
HashedDan <dmangum@hashedhealth.com>
2017-09-23 21:17:24 -0500

    updated factory

--- Collective.sol
+++ Collective.sol
@@ -101,8 +101,15 @@ contract Collective {
 
 contract  CollectiveFactory {
 
-	function createCollective(bool _isSponsor, address[] _sponsors, address[] _individuals, bytes32[] _milestoneNames, uint[] _milestonePayoutDays, uint[] _milestonePayoutPercentages, uint[] _milestoneSteps) returns (Collective collectiveAddr) {
-		return new Collective(_isSponsor,_sponsors,_individuals,_milestoneNames,_milestonePayoutDays,_milestonePayoutPercentages,_milestoneSteps);
+	address public lastDeployed;
+
+	function createCollective(bool _isSponsor, address[] _sponsors, address[] _individuals, bytes32[] _milestoneNames, uint[] _milestonePayoutDays, uint[] _milestonePayoutPercentages, uint[] _milestoneSteps) returns (Collective) {
+
+		Collective newContract = new Collective(_isSponsor,_sponsors,_individuals,_milestoneNames,_milestonePayoutDays,_milestonePayoutPercentages,_milestoneSteps);
+
+		lastDeployed = newContract;
+
+		return newContract;
 	}
 
 }
