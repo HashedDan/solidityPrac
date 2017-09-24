ac683a4215c48f47b7b1dc45062295971589a8ba
HashedDan <dmangum@hashedhealth.com>
2017-09-23 21:23:23 -0500

    separated collective factory into own contract

--- Collective.sol
+++ Collective.sol
@@ -97,19 +97,3 @@ contract Collective {
 	}
 
 }
-
-
-contract  CollectiveFactory {
-
-	address public lastDeployed;
-
-	function createCollective(bool _isSponsor, address[] _sponsors, address[] _individuals, bytes32[] _milestoneNames, uint[] _milestonePayoutDays, uint[] _milestonePayoutPercentages, uint[] _milestoneSteps) returns (Collective) {
-
-		Collective newContract = new Collective(_isSponsor,_sponsors,_individuals,_milestoneNames,_milestonePayoutDays,_milestonePayoutPercentages,_milestoneSteps);
-
-		lastDeployed = newContract;
-
-		return newContract;
-	}
-
-}
---
+++ CollectiveFactory.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.11;
+
+import "./Collective";
+
+contract  CollectiveFactory {
+
+	address public lastDeployed;
+
+	function createCollective(bool _isSponsor, address[] _sponsors, address[] _individuals, bytes32[] _milestoneNames, uint[] _milestonePayoutDays, uint[] _milestonePayoutPercentages, uint[] _milestoneSteps) returns (Collective) {
+
+		Collective newContract = new Collective(_isSponsor,_sponsors,_individuals,_milestoneNames,_milestonePayoutDays,_milestonePayoutPercentages,_milestoneSteps);
+
+		lastDeployed = newContract;
+
+		return newContract;
+	}
+
+}
