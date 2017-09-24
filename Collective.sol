cd809338b2d35e22c6a179ef2693f16458a7ed25
HashedDan <dmangum@hashedhealth.com>
2017-09-23 19:42:13 -0500

    collective contract and factory

---
+++ Collective.sol
@@ -0,0 +1,76 @@
+pragma solidity ^0.4.11;
+
+
+contract Collective {
+
+	struct Milestone {
+		bytes32 name;
+		uint start;
+		uint daysToPayout;
+		uint percentPayout;
+	}
+
+	CollectiveFactory factory;
+	address[] public sponsors;
+	address[] public individuals;
+	address public initializer;
+	Milestone[] public milestones;
+	uint public numMilestones;
+	uint public startDate;
+	uint public endDate;
+
+	function Collective(bool _isSponsor, address[] _sponsors, address[] _individuals, bytes32[] _milestonesNames, uint[] _milestonePayoutDays, uint[] _milestonePayoutPercentages) {
+		
+		startDate = now;
+
+		if (_isSponsor) {
+			sponsors.push(tx.origin);
+		}
+		else {
+			individuals.push(tx.origin);
+		}
+
+		for (uint i = 0; i < _sponsors.length; ++i) {
+			sponsors.push(_sponsors[i]);
+		}
+
+		for (uint j = 0; j < _individuals.length; ++j) {
+			individuals.push(_individuals[j]);
+		}
+
+		for (uint k = 0; k < _milestonesNames.length; ++k) {
+			createMilestone(_milestonesNames[k], startDate, _milestonePayoutDays[k], _milestonePayoutPercentages[k]);
+		}
+
+		initializer = tx.origin;
+
+		factory = CollectiveFactory(msg.sender);
+	}
+
+	function createMilestone(bytes32 _name, uint _daysToPayout, uint _percentPayout) {
+		milestones.push(Milestone(_name, startDate, _daysToPayout, _percentPayout));
+	}
+
+	function payoutMilestone() {
+
+	}
+
+	function getMilestones() constant returns (bytes32[], uint[]) {
+		bytes32[] memory _milestoneNames = new bytes32(milestones.length);
+		uint[] memory _milestonePayoutDaysRemaining = new uint(milestones.length);
+
+		for (uint i = 0; i < milestones.length; ++i) {
+			_milestoneNames[i] = milestones[i].name;
+		}
+	}
+
+}
+
+
+contract  CollectiveFactory {
+
+	function createCollective() returns (Collective collectiveAddr) {
+		return new Collective();
+	}
+
+}
